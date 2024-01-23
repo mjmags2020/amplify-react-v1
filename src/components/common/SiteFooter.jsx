952e65103980b72cbdda2fc795a92e997760fd0c
Mark Magparoc <mjmags2020@gmail.com>
2024-01-23 16:06:02 +0800

    Added a Route and  Default pages

---
+++ src/components/common/SiteFooter.jsx
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container';
+
+function SiteFooter() {
+    return (
+        <footer className="py-5 my-5">
+            <Container className="px-4">
+                <p className="text-end">
+                    <br />&copy; 2024 Ascendware&nbsp;&nbsp;
+                </p>
+            </Container>
+        </footer>
+    )
+}
+
+export default SiteFooter;
